0d66ed27e053221fd2049353ac566646e8dd1141
vcvabs8 <104188919+vcvabs8@users.noreply.github.com>
2022-04-25 12:28:02 +0530

    Update simplegit.java
    first m1 method for pull by system

--- git01/src/git01/simplegit.java
+++ git01/src/git01/simplegit.java
@@ -1,11 +1,18 @@
 package git01;
 
 public class simplegit {
+	public void m1()
+	{
+		System.out.println("This is my first method to pull");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		simplegit s=new simplegit();
 		System.out.println("Hello Nil");
+		s.m1();
+		
 	}
 
 }
